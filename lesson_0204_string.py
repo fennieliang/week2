f3992676a28b768f53ef9c60e62a775bb15b1e3c
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:09:59 +0800

    Create lesson_0204_string.py

---
+++ lesson_0204_string.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 2 16:03:37 2021
+
+@author: fennieliang
+"""
+    
+#String
+a='This'
+b='is'
+c='a Python practice!'
+
+print (c)          # Prints the complete string c
+print (c[0])       # Prints the first character of c
+print (c[0:5])     # Prints characters starting from the first to the 5th
+print (c[2:])      # Prints characters starting from the 3rd character  to the end
+print (c * 2)      # Prints c twice
+
+print (f"a+b+c = {a+' '+b+' '+c}") #text can be concatenated using '+' delimiter
+
+
+#practice: Try string a - string b
+#for example 
+a = 'Hello World'
+b = 'Hello'
+print (f"a - b = {a-b}")
+
+
+
+
