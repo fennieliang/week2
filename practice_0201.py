013b2793440754f3f562dea21aed511183cb2801
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:09:51 +0800

    Create practice_0201.py

---
+++ practice_0201.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  9 11:15:46 2021
+
+@author: fennieliang
+"""
+
+n1 = input('Enter the first number: ')
+n2 = input('Enter the second number: ')
+
+
+sum =int(float(n1)+float(n2))
+print (f"n1 + n2 = {sum}")#Check the result 
+
+
