0343097ed47569bafd2e2fb0a931724cba57c1f5
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:09:37 +0800

    Create lesson_0201_number.py

---
+++ lesson_0201_number.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 2 16:03:37 2021
+
+@author: fennieliang
+"""
+#manually create an anaconda Virtual Environment:
+    #    conda create -n NLP
+    #    conda activate NLP
+    #    deactivate a VM: at current vm type "conda deactivate"
+    #    remove a VM: conda env remove -n NLP
+    #practice: create a new VM called test and see what happens aftward
+
+#Integer
+a=1;b=2
+#print (f'1. a + b = {a+b}')
+#print ("\n")
+
+#Float
+a=38.5  
+#print (f'1. a + b = {a+b}')
+#print ("\n")
+
+b=1e+3 #1e means 10^, le+3 means 10^3 and so on 
+#print (f"3. b = {b}")#because b is a float, the result needs to be 1000.0 not 1000
+#print ("\n")
+
+#print (f"4. a + b = {a+b}") 
+#print ("\n")
+
+sum = int(a+b)
+print ("the result is ", sum) #This is an old version of printing results 
+#print ("\n")
+
+'''
+#practice 0201 take two input integers and print the sum up result
+
+'''
+
+'''
+#practice 0202 take input number for 5 times and print the result
+  
+'''  
+ 
+'''
+#practice 0203 print a 20 layers of a * triangle
+  
+''' 
