0cf80dc9055bec4a2af397e118f7aaec8820f97d
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:09:48 +0800

    Create practice_0202.py

---
+++ practice_0202.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  9 11:17:53 2021
+
+@author: fennieliang
+"""
+
+
+y = 0
+for i in range(1,6):
+    x = input('Enter a number:')
+    y = y+int(x)
+print (f"the result = {y}")
