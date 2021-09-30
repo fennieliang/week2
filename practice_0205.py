37cae90e4489568595dbfd96285cbe12713cba5b
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:09:40 +0800

    Create practice_0205.py

---
+++ practice_0205.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 10:53:21 2021
+
+@author: fennieliang
+"""
+
+a=['apple','melon','orange','pine apple']
+
+for i in range(3):
+    print (a[i])
