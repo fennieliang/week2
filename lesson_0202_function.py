43901f2e7339461c11bdd2f48a50ab724b9073f4
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:10:05 +0800

    Create lesson_0202_function.py

---
+++ lesson_0202_function.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 10:22:44 2021
+
+@author: fennieliang
+"""
+
+def max(a,b):
+    return a if a>b else b
+    
+def min(a,b):
+    return a if a<b else b
+
+
+
+big = max(3,5)
+small = min(3,5)
+print (f"max number:{big}")
+print (f"min number:{small}")
+
+'''
+practice 0204 find the max number from three digits
+
+'''
